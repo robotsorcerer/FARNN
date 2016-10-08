382a7f16eefe5827b0726408085caa3e3d6afd8a
Lekan <lakehanne@users.noreply.github.com>
2016-10-07 19:18:14 -0500

    Add files via upload

--- sample.lua
+++ sample.lua
@@ -1,11 +1,12 @@
 require 'torch'
 require 'nn'
 require 'rnn'
+require 'nngraph'
 
 local cmd = torch.CmdLine()
 --Gpu settings
 cmd:option('-gpu', 0, 'which gpu to use. -1 = use CPU; >=0 use gpu')
-cmd:option('-checkpoint', 'network/lstm-net.t7', 'load the trained network e.g. <lstm-net.t7| rnn-net.t7|mlp-net.t7>')
+cmd:option('-checkpoint', 'sr-net/softRobot_fastlstm-net.t7', 'load the trained network e.g. <lstm-net.t7| rnn-net.t7|mlp-net.t7>')
 cmd:option('-backend', 'cudnn', 'nn|cudnn')
 cmd:option('-verbose', 0)
 
@@ -36,9 +37,32 @@ if use_cuda then
 else
 	model = torch.load(opt.checkpoint)
 end
-print('model', model)
+
+netmods = model.modules;
+print('netmods: \n', netmods)
+
+weights,biases = {}, {};
+netparams = {}
+
+--[[This if .. then below takes care of recurrent modules ]]
+-- if (function(model) return model:find('nn.Recursor') end) then --recurrent nngraph module
+if #netmods == 1 then   		--recurrent modules
+	local modules 	= netmods[1].recurrentModule.modules
+	local length 	= #netmods[1].recurrentModule.modules
+	for i = 1, length do
+		netparams[i] 	= {['weight']=modules[i].weight, ['bias']=modules[i].bias}
+	end
+
+	print('\nnetparams\n:'); 
+	print(netparams)
+elseif #netmods > 1 then   --mlp modules
+	for i = 1, #netmods do		
+		netparams[i] 	= {['weight']=netmods[i].weight, ['bias']=netmods[i].bias}
+	end
+
+	print('\nnetparams\n:'); 
+	print(netparams)
+end
+
 
 model:evaluate()
-
-local sample = model:sample(opt)
-print('sample', '\n', sample)
